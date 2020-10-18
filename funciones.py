75c55a1e7fdba6312ef9ce33433a5d2a98566234
Sthefano <sthefanou25@gmail.com>
2020-10-18 16:34:48 -0500

    programa con funciones

---
+++ funciones.py
@@ -0,0 +1,17 @@
+
+def conversacion(mensaje):
+    print('Hola')
+    print('Que tal')
+    print('Elegiste la opcion '+ mensaje)
+    print('De nada, vuelva pronto 🤠')
+
+opcion = int(input('Elige una opcion (1, 2, 3): '))
+
+if opcion == 1:
+    conversacion('1')
+elif opcion == 2:
+    conversacion('2')
+elif opcion == 3:
+    conversacion('3')
+else:
+    print('No escogiste ninguna opcion, me da amsiedad 🐶')
